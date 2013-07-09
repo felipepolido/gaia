9abe98a9e5a32a0d75e71a3382f1f1dce0190288
ekolker <e.kolker@gmail.com>
2013-07-08 20:42:03 -0400

    thats all, folks

--- src/laser_processor/scripts/laser_processor.py
+++ src/laser_processor/scripts/laser_processor.py
@@ -6,22 +6,38 @@
 
 
 import rospy
+
 # from geometry_msgs.msg import TwistWithCovarianceStamped
 # from geometry_msgs.msg import TwistWithCovariance
 # from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
+from geometry_msgs.msg import Pose
+
 from std_msgs.msg import String
 from std_msgs.msg import Header
+
 # from sensor_msgs.msg import Joy
 from sensor_msgs.msg import LaserScan
 from sensor_msgs.msg import JointState
 
+from nav_msgs.msg import Odometry
 
 
 
 
-def callback(data):
-	pass
+
+
+def gazebo_laser_callback(data, args):
+	'''
+	Callback for the gazebo-based (sim) laser found on the front of the robot.
+
+	args: list of things! current contents:
+		0	publisher to use
+		1	pose of scanner wrt the world
+
+	'''
+	
+
 
 
 
@@ -36,10 +52,20 @@ def laser_processor():
 	# initalize the node
 	rospy.init_node('laser_processor')
 
-	# init a listener for the joystick
-	# sub = rospy.Subscriber("joy", Joy, callback)
-	
 
+	# 	set up the listener for raw laserscans
+
+	# for now we assume just the publisher is needed as an arg, but more soon?
+	# todo: get lidar pose wrt the world (map?)
+	gazebo_scan_callback_args = [laser_publisher]
+
+	# init a listener for the gazebo laser
+	gazebo_laser_sub = rospy.Subscriber("/gaia_gazebo_plugin/base_scan", \
+		LaserScan, gazebo_laser_callback, gazebo_scan_callback_args)
+
+	gazebo_pose_sub = rospy.Subscriber("/gaia_gazebo_plugin/odom", \
+
+	
 
 	print "Firin' mah lazer!"
 	rospy.spin()
